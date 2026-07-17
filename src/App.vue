eee4b255ecb4aceacd82f0184c17f48064150f73
Wokrlie <hehelilibz@gmail.com>
2026-07-17 20:43:38 +0800

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts"></script>
+
+<template>
+  <h1>Hello, welcome to the "EasyToChat"!</h1>
+</template>
+
+<style scoped>
+h1 {
+  font-family: system-ui, monospace;
+}
+p {
+  font-family: system-ui, monospace;
+}
+</style>
