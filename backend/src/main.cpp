2370da1a26d0081797c68b93109749b86a6db3f4
Wokrlie <hehelilibz@gmail.com>
2026-07-18 15:19:31 +0800

    feat: use crow create a backend

---
+++ backend/src/main.cpp
@@ -0,0 +1,14 @@
+#include <crow/app.h>
+#include <crow/http_response.h>
+#include <string>
+
+int main() {
+    crow::SimpleApp app;
+
+    CROW_ROUTE(app, "/api/ping")
+        ([]() {
+            return crow::response("pong");
+        });
+
+    app.port(31455).multithreaded().run();
+}
